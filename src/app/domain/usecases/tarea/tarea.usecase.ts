984505484b873a3bcfb1ede2ec868a11d12e30ee
DanielChambueta08 <drosorio@bancolombia.com.co>
2024-01-25 21:41:05 -0500

    [ADD] Use Case for Tarea - only listarTareas

---
+++ src/app/domain/usecases/tarea/tarea.usecase.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { TareaModel } from '../../models/tarea/tarea.model';
+import { TareaGateway } from '../../models/tarea/tarea.gateway';
+
+
+@Injectable()
+export class TareaUsecase {
+
+  constructor(private tareaGateway: TareaGateway) { }
+
+  listarTareasUC(): Observable<TareaModel[] | null> {
+    return this.tareaGateway.listarTareasG();
+  }
+}
